7e591e0029752e18070021331dcc2607a48b9517
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2017-02-12 19:49:44 +0900

    img側エンジンの構造体を作成

--- levena.c
+++ levena.c
@@ -3,6 +3,17 @@
 //ibus.hにはibusengine.hやらglibc.hやらがincludeされてる
 #include<ibus.h>
 
+typedef struct tagIBusLevenaEngine IBusLevenaEngine;
+typedef struct tagIBusLevenaEngineClass IBusLevenaEngineClass;
+
+struct tagIBusLevenaEngine{
+
+};
+
+struct tagIBusLevenaEngineClass{
+
+};
+
 int main(){
 
 }
