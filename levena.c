e5841f75c82c292a15618cf584f99c542c94d198
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2017-02-12 20:13:48 +0900

    ibus_initの追加。多分ibus_katanaもこいつがないから落ちた。

--- levena.c
+++ levena.c
@@ -16,12 +16,20 @@ struct tagIBusLevenaEngineClass{
 
 int main(int argc,char **argv){
 
+ibus_init();//絶対必要っぽい。忘れてた。
+
 //必要なibus_系の宣言
 IBusBus *bus;
 IBusFactory *factory;
+//component: imeエンジンの説明、作者、バージョン等。通常xmlから読み込む
+IBusComponent *component;
 gchar *IMEname="levena";
+
 
 bus=ibus_bus_new();
 factory=ibus_factory_new(ibus_bus_get_connection(bus));
+//todo:下の理解。ime側エンジンなのこれ、imエンジンなの？
 ibus_factory_create_engine(factory,IMEname);
+
+
 }
