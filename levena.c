736bd146ac07b128b00a86915d963fd41a33359b
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2017-02-12 20:02:38 +0900

    bus,factoryの作成、factoryにengine追加。

--- levena.c
+++ levena.c
@@ -19,5 +19,9 @@ int main(int argc,char **argv){
 //必要なibus_系の宣言
 IBusBus *bus;
 IBusFactory *factory;
+gchar *IMEname="levena";
 
+bus=ibus_bus_new();
+factory=ibus_factory_new(ibus_bus_get_connection(bus));
+ibus_factory_create_engine(factory,IMEname);
 }
