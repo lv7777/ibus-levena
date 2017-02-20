61f2155a339c50eb318702108cff22535ff0bb1e
oonuma ryouyu <oonuma.ry0uyu@gmail.com>
2017-02-20 22:52:12 +0900

    fix syntax error

--- levena.c
+++ levena.c
@@ -23,19 +23,6 @@ void ibus_levena_engine_destroy(IBusLevenaEngine *klass){
     g_printf("end!!");
 }
 
-typedef struct tagIBusLevenaEngine IBusLevenaEngine;
-typedef struct tagIBusLevenaEngineClass IBusLevenaEngineClass;
-
-//インスタンス変数等を登録する。
-struct tagIBusLevenaEngine{
-    IBusEngine parent;
-};
-
-//こっちはメソッドやらクラス変数を登録する。
-struct tagIBusLevenaEngineClass{
-    IBusEngineClass parent;
-};
-//上記２つは公開している。protectedやprivateじゃない。
 
 //catch the process-key-event signal from ibus_init
 gboolean levena_process_key_event(IBusEngine *ie,guint keyval,guint keycode,guint state,gpointer user_data){
@@ -69,7 +56,7 @@ factory=ibus_factory_new(ibus_bus_get_connection(bus));
 //とりあえずibus_factory_create_engineとibus_factory_add_engineは違う。
 //ibus_factory_create_engine(factory,IMEname);
 //TODO:engineタイプの追加。
-ibus_factory_add_engine(factory,IMEname,);
+ibus_factory_add_engine(factory,IMEname,IBUS_TYPE_LEVENA_ENGINE);
 
 iec=IBUS_ENGINE_CLASS (levenaengine);
 iec->process_key_event=levena_process_key_event;
